c023369dd99ee1159dd5afb36d75375a2e8fe23c
Saikrishna.V <94283611+saikrishna2003@users.noreply.github.com>
2023-03-06 10:32:55 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/example/demo/ReactSpringEmployeeMananagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringEmployeeMananagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringEmployeeMananagementSystemApplication.class, args);
+	}
+
+}
